8ceda47ac081fcda82cf9a278662bd06591cc38b
Subhani Sayyad <65666841+subhani9@users.noreply.github.com>
2023-11-01 18:20:54 +0530

    done

---
+++ neha/src/components/layout/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../layout/Layout.css";
+import { Helmet } from "react-helmet";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Helmet>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+      <main>{children}</main>
+    </div>
+  );
+};
+Layout.defaultProps = {
+  title: "Meenamma",
+  description:
+    "Im sorry meenamma nuv kavali manchiga unta manchiga chuskunta inkeppudu em ananu",
+  keywords: "neha, meena,meenamma, subbu, subhani,meena loves subbu",
+};
+export default Layout;
